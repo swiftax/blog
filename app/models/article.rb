b774662685971cf14e4fe64dc111679f4cd9a955
Anthony Tropeano <tony@swiftax.com>
2015-07-02 16:46:50 -0400

    devise and styling updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-	has_many  :comments 
+	has_many  :comments
+	belongs_to :user 
 	validates :title, presence: true,
 						length: { minimum: 5 }
 	validates :text, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   resources :articles do
   	resources :comments
   end
 
   root 'welcome#index'
+
+  devise_for :users
+  resources :users
+
+
 end
+
