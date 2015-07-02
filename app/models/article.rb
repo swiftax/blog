0635aae0ce8d6490a171330ae7ac96f67572633c
Anthony Tropeano <tony@swiftax.com>
2015-07-02 17:11:44 -0400

    created name for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	has_many  :comments
+	has_many  :comments, dependent: :destroy
 	belongs_to :user 
 	validates :title, presence: true,
 						length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702201016) do
+ActiveRecord::Schema.define(version: 20150702211010) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "comments", force: :cascade do |t|
